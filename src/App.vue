770aa1c9d3b22931b3a5ead5d3c9c82c40faec5e
ryanjavens <raykon1499@gmail.com>
2021-05-26 18:40:21 -0400

    Replaced tailwind with bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
-  <div class="container mx-auto">
-    <h1>hello</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1>RestedXP Addon Guide Editor</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-6 d-grid gap-2 mx-auto">
+        <button type="button" class="btn btn-lg btn-primary">New Guide</button>
+        <button type="button" class="btn btn-lg btn-primary">Import Guide</button>
+      </div>
+    </div>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './index.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 
 createApp(App).mount('#app')
