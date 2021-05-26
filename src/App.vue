56c3f6f9f292eda7097eafe7ba0a8ff1a9171964
ryanjavens <raykon1499@gmail.com>
2021-05-26 19:41:18 -0400

    Added link to RXP site

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col">
-        <h1>RestedXP Addon Guide Editor</h1>
-      </div>
+  <div class="row">
+    <div class="col">
+      <h1>RestedXP Addon Guide Editor</h1>
     </div>
-    <div class="row">
-      <div class="col-6 d-grid gap-2 mx-auto">
-        <button type="button" class="btn btn-lg btn-primary">New Guide</button>
-        <button type="button" class="btn btn-lg btn-primary disabled">Import Guide - Coming soon</button>
-      </div>
+  </div>
+  <div class="row">
+    <div class="col-md-6 d-grid gap-2 mx-auto">
+      <button type="button" class="btn btn-lg btn-primary">New Guide</button>
+      <button type="button" class="btn btn-lg btn-primary disabled">Import Guide - Coming soon</button>
     </div>
   </div>
 </template>
@@ -22,11 +20,7 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
